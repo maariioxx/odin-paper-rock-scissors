f1caf5e3908c3192315a30a19a6ad177a125ce51
maariioxx <mariorb2017@gmail.com>
2023-10-25 12:43:49 +0200

    Add playerChoice

--- main.js
+++ main.js
@@ -3,17 +3,36 @@ let paper = "papel";
 let scissors = "tijeras";
 
 function getComputerChoice() {
-    let computerChoice;
-    selection = Math.floor(Math.random() * 3);
-    if (selection === 0){
-        computerChoice = rock;
-        return computerChoice;
-    } else if (selection === 1){
-        computerChoice = paper;
-        return computerChoice;
+    let computerSelecion;
+    computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice === 0){
+        computerSelecion = rock;
+        return computerSelecion;
+    } else if (computerChoice === 1){
+        computerSelecion = paper;
+        return computerSelecion;
     } else {
-        computerChoice = scissors;
-        return computerChoice;
+        computerSelecion = scissors;
+        return computerSelecion;
     }
 }
 
+
+let playerSelection = "";
+while (playerSelection != rock && playerSelection != paper && playerSelection != scissors){
+    playerChoice = prompt("Elige piedra, papel o tijeras para jugar contra la máquina").toLowerCase();
+    if (playerChoice === "piedra"){
+        playerSelection = rock;
+        break
+    } else if (playerChoice === "papel"){
+        playerSelection = paper;
+        break
+    } else if (playerChoice === "tijeras"){
+        playerSelection = scissors;
+        break
+    }
+}
+console.log(playerChoice);
+console.log(playerSelection);
+    
+    
